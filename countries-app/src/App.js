28e67c0c0c6f550e0116e06dc0cf7a533c5ba39c
anhduc109 <ducpham1098@gmail.com>
2020-02-10 11:51:18 +0200

    added sorting function and fixing stuff

--- countries-app/src/App.js
+++ countries-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import lodash from "lodash";
 
 import "./App.css";
 import useFetch from "./custom-hooks/useFetch";
@@ -8,6 +9,9 @@ import CountriesTable from "./components/CountriesTable";
 const App = () => {
   const [countries, error] = useFetch();
   const [filteredCountries, setFilteredCountries] = useState([]);
+  const [isAscName, setIsAscName] = useState(true);
+  const [isAscPopulation, setIsAscPopulation] = useState(true);
+  const [isAscRegion, setIsAscRegion] = useState(true);
 
   useEffect(() => {
     setFilteredCountries(countries);
@@ -21,11 +25,53 @@ const App = () => {
     );
   };
 
+  const handleNameIsAsc = isAsc => {
+    let realIsAsc = !isAsc;
+    setIsAscName(!isAscName);
+    setIsAscPopulation(true);
+    setIsAscRegion(true);
+    setFilteredCountries(
+      lodash.orderBy(filteredCountries, "name", realIsAsc ? "asc" : "desc")
+    );
+  };
+
+  const handleIsAscPopulation = isAsc => {
+    let realIsAsc = !isAsc;
+    setIsAscPopulation(!isAscPopulation);
+    setIsAscName(true);
+    setIsAscRegion(true);
+    setFilteredCountries(
+      lodash.orderBy(
+        filteredCountries,
+        "population",
+        realIsAsc ? "asc" : "desc"
+      )
+    );
+  };
+
+  const handleIsAscRegion = isAsc => {
+    let realIsAsc = !isAsc;
+    setIsAscRegion(!isAscRegion);
+    setIsAscName(true);
+    setIsAscPopulation(true);
+    setFilteredCountries(
+      lodash.orderBy(filteredCountries, "region", realIsAsc ? "asc" : "desc")
+    );
+  };
+
   return !error ? (
     <div className="app-wrapper">
       <h1 className="header">Countries List</h1>
       <SearchBar handleSearchInput={handleSearchInput} />
-      <CountriesTable data={filteredCountries} />
+      <CountriesTable
+        data={filteredCountries}
+        isAscName={isAscName}
+        handleNameIsAsc={handleNameIsAsc}
+        isAscPopulation={isAscPopulation}
+        handleIsAscPopulation={handleIsAscPopulation}
+        isAscRegion={isAscRegion}
+        handleIsAscRegion={handleIsAscRegion}
+      />
     </div>
   ) : (
     <div className="app-wrapper">
--- countries-app/src/components/CountriesTable.js
+++ countries-app/src/components/CountriesTable.js
@@ -2,25 +2,98 @@ import React from "react";
 
 import CountriesRow from "./CountriesRow";
 
-const CountriesTable = ({ data }) => {
+const CountriesTable = ({
+  data,
+  handleNameIsAsc,
+  isAscName,
+  handleIsAscPopulation,
+  isAscPopulation,
+  handleIsAscRegion,
+  isAscRegion
+}) => {
   return (
     <table className="country-table">
       <thead>
         <tr>
           <th>Flag</th>
-          <th>Name</th>
+          <th>
+            Name
+            {isAscName ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="arrow-symbol"
+                viewBox="0 0 24 24"
+                onClick={() => handleNameIsAsc(isAscName)}
+              >
+                <path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z" />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="arrow-symbol"
+                viewBox="0 0 24 24"
+                onClick={() => handleNameIsAsc(isAscName)}
+              >
+                <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
+              </svg>
+            )}
+          </th>
           <th>Languages</th>
-          <th>Population</th>
-          <th>Region</th>
+          <th>
+            Population
+            {isAscPopulation ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="arrow-symbol"
+                viewBox="0 0 24 24"
+                onClick={() => handleIsAscPopulation(isAscPopulation)}
+              >
+                <path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z" />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="arrow-symbol"
+                viewBox="0 0 24 24"
+                onClick={() => handleIsAscPopulation(isAscPopulation)}
+              >
+                <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
+              </svg>
+            )}
+          </th>
+          <th>
+            Region
+            {isAscRegion ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="arrow-symbol"
+                viewBox="0 0 24 24"
+                onClick={() => handleIsAscRegion(isAscRegion)}
+              >
+                <path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z" />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="arrow-symbol"
+                viewBox="0 0 24 24"
+                onClick={() => handleIsAscRegion(isAscRegion)}
+              >
+                <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
+              </svg>
+            )}
+          </th>
         </tr>
       </thead>
       <tbody>
-        {data.map((country, index) => {
-          return <CountriesRow data={country} key={index} />;
+        {data.map(country => {
+          return <CountriesRow data={country} key={country.name} />;
         })}
       </tbody>
     </table>
   );
 };
+
+CountriesTable.displayName = "CountriesTable";
 
 export default CountriesTable;
